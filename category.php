e48a147ec7da17e8769cdb2e856dbb2076d386de
Jeff Wang <wangchj@ilab.auburn.edu>
2014-05-07 23:38:11 -0500

    Remove 'Category Archive' label from the top of category page.

--- category.php
+++ category.php
@@ -16,18 +16,6 @@ get_header(); ?>
 
 			<?php if ( have_posts() ) : ?>
 
-			<header class="archive-header">
-				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentyfourteen' ), single_cat_title( '', false ) ); ?></h1>
-
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .archive-header -->
-
 			<?php
 					// Start the Loop.
 					while ( have_posts() ) : the_post();
